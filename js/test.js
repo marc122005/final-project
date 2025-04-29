9ccaec5b0a4209beabeb1d927d4c21f6d2cd039e
marc122005 <marcamgad12@gmail.com>
2025-04-29 03:34:43 +0300

    created main files

---
+++ js/test.js
@@ -0,0 +1,13 @@
+
+let friends =["marc", "paula", "john", "anton", "kevin", "bavly"];
+
+
+
+friends.unshift("makoo");
+
+friends.push("mohamed");
+
+document.writeln(friends);
+document.writeln("<br>");
+document.writeln(friends.length);
+
